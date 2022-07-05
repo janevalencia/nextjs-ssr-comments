409832d7c0bd5fa8c9219df51fad387de03b67a6
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-07-05 18:24:10 +0800

    Create a reusable & generic Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,92 @@
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faInstagram, faLinkedin, faTwitch, faTwitter, faYoutube, faMedium } from "@fortawesome/free-brands-svg-icons";
 
-const Footer = () => {
+// Default Footer props.
+const defaultFooterProps = {
+  author: 'Jane Valencia',
+  year: new Date().getFullYear().toString(),
+}
+
+// Define social media props.
+type FooterProps = { 
+  github? : string,
+  linkedin? : string,
+  instagram? : string,
+  twitter? : string,
+  youtube? : string,
+  twitch? : string,
+  medium? : string,
+} & typeof defaultFooterProps;
+
+const Footer = ( props : FooterProps) => {
   return (
-    <footer>
-      {/* Author */}
-      <section>
-        <div>
+    <footer className="footer p-8">
+      <section className="footer__main flex flex-col md:flex-row justify-between my-4">
+        {/* Author, Copyright, Brand */}
+        <div className="footer__brand-copyright">
           <span>Frontend Mentor Challenge</span>
-          <p>
-            Solution developed by Jane Valencia &copy; 2022 All rights reserved.
-          </p>
+          <p>Copyright &copy; { props.year } { props.author }. All rights reserved.</p>
         </div>
-        <div>
-          <h3>Follow me</h3>
-          <div className="footer__socials">
-            <a className="footer__social-link">IG</a>
-            <a className="footer__social-link">LI</a>
-            <a className="footer__social-link">GitHub</a>
+
+        {/* Social Links */}     
+        <div className="footer__socials">
+          <h3>Let's connect!</h3>
+          <div className="footer__socials-links">
+            {/* GitHub */}
+            { props.github && (
+              <a className="footer__social-link" href={ props.github }>
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            ) }
+
+            {/* LinkedIn */}
+            { props.linkedin && (
+              <a className="footer__social-link" href={ props.linkedin }>
+                <FontAwesomeIcon icon={ faLinkedin } />
+              </a>
+            ) }
+
+            {/* Instagram */}
+            { props.instagram && (
+              <a className="footer__social-link" href={ props.instagram }>
+                <FontAwesomeIcon icon={ faInstagram } />
+              </a>
+            ) }
+
+            {/* Twitter */}
+            { props.twitter && (
+              <a className="footer__social-link" href={ props.twitter }>
+                <FontAwesomeIcon icon={ faTwitter } />
+              </a>
+            ) }
+
+            {/* YouTube */}
+            { props.youtube && (
+              <a className="footer__social-link" href={ props.youtube }>
+                <FontAwesomeIcon icon={ faYoutube } />
+              </a>
+            ) }
+
+            {/* Twitch */}
+            { props.twitch && (
+              <a className="footer__social-link" href={ props.twitch }>
+                <FontAwesomeIcon icon={ faTwitch } />
+              </a>
+            ) }
+
+            {/* Medium */}
+            { props.medium && (
+              <a className="footer__social-link" href={ props.medium }>
+                <FontAwesomeIcon icon={ faMedium } />
+              </a>
+            ) }
           </div>
         </div>
       </section>
 
       {/* Vercel */}
-      <section>
+      <section className="footer__poweredby">
         <a
           className="flex items-center justify-center gap-2"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -36,5 +100,6 @@ const Footer = () => {
     </footer>
   )
 }
+Footer.defaultProps = defaultFooterProps;
 
 export default Footer;
