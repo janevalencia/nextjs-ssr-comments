dd47c1c0c38294d00292cf7157498e6d209e4cd1
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-07-05 15:58:33 +0800

    Add @fontawesome modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
 import "../styles/globals.scss";
 import { MainLayout } from "../components";
 import type { AppProps } from "next/app";
+
+// FontAwesome: Ensure the SVG library is not added to the HEAD-meta of the page.
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
