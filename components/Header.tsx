31ec4a5d8583ffd9015a7d55c523b241cc3cbd6c
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-07-05 14:30:16 +0800

    Create a reusable & generic Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,24 @@
-const Header = () => {
+import { ReactNode } from "react";
+
+// Define default Brand / Logo props.
+const defaultBrandProp = {
+    brand: <h1>FEM Interactive Comments</h1>,
+};
+
+// Define Header props.
+type HeaderProps = { children? : ReactNode } & typeof defaultBrandProp;
+
+// Header component.
+const Header = ( { brand, children } : HeaderProps ) => {
     return (
-        <header>
-            fem-comments
+        <header className="w-full flex justify-center md:justify-start p-4">
+            { brand }
+            <div>
+                { children }
+            </div>
         </header>
     );
 }
+Header.defaultProps = defaultBrandProp;
 
 export default Header;
