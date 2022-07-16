2b0de6f66970ee914c02354661df95c816df50b0
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-07-16 23:46:08 +0800

    Temp data (to be deleted once api setup)

--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Vote } from "../components";
 
+// To be deleted later once DB is setup.
 const SampleUser = {
     "id": 1,
     "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
@@ -13,7 +14,34 @@ const SampleUser = {
       "username": "amyrobson",
       "active": true,
     },
-    "replies": []
+    "replies": [
+        {
+          "id": 3,
+          "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          "createdAt": "1 week ago",
+          "score": 4,
+          "replyingTo": "maxblagun",
+          "user": {
+            "image": { 
+              "png": "./images/avatars/image-ramsesmiron.png"
+            },
+            "username": "ramsesmiron"
+          }
+        },
+        {
+          "id": 4,
+          "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          "createdAt": "2 days ago",
+          "score": 2,
+          "replyingTo": "ramsesmiron",
+          "user": {
+            "image": { 
+              "png": "./images/avatars/image-juliusomo.png"
+            },
+            "username": "juliusomo"
+          }
+        }
+    ]
 }
 
 const CommentCard = () => {
