bc9578f2ad5cf5e350abaedd768cf3e8d012b1c8
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-07-16 23:37:59 +0800

    Make CommentCard mobile-responsive

--- components/CommentCard.tsx
+++ components/CommentCard.tsx
@@ -11,7 +11,7 @@ const SampleUser = {
         "png": "/assets/images/image-amyrobson.png"
       },
       "username": "amyrobson",
-      "active": false,
+      "active": true,
     },
     "replies": []
 }
@@ -19,12 +19,12 @@ const SampleUser = {
 const CommentCard = () => {
     const { id, content, createdAt, score, user, replies } = SampleUser;
     return (
-        <div className="comment-card grid grid-flow-col grid-rows-3 gap-x-4 gap-y-2">
-            <div className="row-span-3 col-end-1">
+        <div className="comment-card grid grid-flow-col md:grid-rows-3 gap-x-4 gap-y-2">
+            <div className="row-start-2 md:row-span-3 col-end-1">
                 <Vote votes={ score } />
             </div>
-            <div className="comment-card__user col-start-1">
-                <div className="flex flex-row justify-start items-center gap-2">
+            <div className="comment-card__user md:row-start-1 row-end-1 col-span-4 md:col-span-1">
+                <div className="flex flex-row justify-start items-center gap-2 pt-1">
                     <div className="comment-card__user profile-img relative overflow-hidden flex items-center">
                         <Image 
                             src={ user.image.png } 
@@ -38,14 +38,14 @@ const CommentCard = () => {
                     <span className="comment-card__user date">{ createdAt }</span>
                 </div>
             </div>
-            <div className="comment-card__cta col-end-4 flex items-center">
+            <div className="comment-card__cta row-start-2 md:row-start-1 col-end-4 flex">
                 { ! user.active ? (
                     <a href="" className="button button__reply flex flex-row items-center gap-x-2">
                         <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
                         Reply
                     </a>
                 ) : (
-                    <div className="flex flex-row justify-end gap-4">
+                    <div className="flex flex-row justify-end gap-6">
                         <a href="" className="button button__delete flex flex-row items-center gap-x-2">
                             <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368"/></svg>
                             Delete
@@ -57,7 +57,7 @@ const CommentCard = () => {
                     </div>
                 )}
             </div>
-            <div className="comment-card__content row-span-2 col-span-3">
+            <div className="comment-card__content md:row-span-2 col-span-4 md:col-span-3">
                 <p>{ content }</p>
             </div>
         </div>
