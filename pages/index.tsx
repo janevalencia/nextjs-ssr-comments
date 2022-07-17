0ac729700c4c027556f4a0df32eb1844addf2246
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-07-18 00:58:55 +0800

    Update SampleData

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,41 @@ import Head from "next/head";
 import { CommentCard, CommentForm } from "../components";
 
 // To be deleted later once DB is setup.
-const SampleData = [{
-  "id": "1",
-  "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-  "timestamp": "1 month ago",
-  "votes": 12,
-  "user": {
-    "image": { 
-      "png": "/assets/images/image-amyrobson.png"
-    },
-    "username": "amyrobson",
-    "loggedInUser": false,
+const CurrentUser = {
+  "image": { 
+    "png": "/assets/images/image-juliusomo.png"
   },
-  "replies": [
+  "username": "juliusomo",
+  "loggedInUser": true,
+}
+const SampleData = [
+  {
+    "id": "1",
+    "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+    "timestamp": "1 month ago",
+    "votes": 12,
+    "user": {
+      "image": { 
+        "png": "/assets/images/image-amyrobson.png"
+      },
+      "username": "amyrobson",
+      "loggedInUser": false,
+    },
+    "replies": []
+  },
+  {
+    "id": "2",
+    "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+    "timestamp": "2 weeks ago",
+    "votes": 5,
+    "user": {
+      "image": { 
+        "png": "/assets/images/image-maxblagun.png"
+      },
+      "username": "maxblagun",
+      "loggedInUser": false,
+    },
+    "replies": [
       {
         "id": "3",
         "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
@@ -44,8 +66,9 @@ const SampleData = [{
           "loggedInUser": true,
         }
       }
-  ]
-}]
+    ]
+  }
+]
 
 const Home: NextPage = () => {
   return (
@@ -63,7 +86,7 @@ const Home: NextPage = () => {
           ) )}
         </section>
         <section className="comments-new">
-          <CommentForm />
+          <CommentForm user={ CurrentUser } />
         </section>
       </main>
     </>
