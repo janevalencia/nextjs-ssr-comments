625e1ff1c35f58a4bfff6ed9a22055719b2d7ca8
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-09-02 00:19:25 +0800

    GET: Users

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,1 +1,37 @@
-// TBA: API to get all users
+/**
+ * API GET: All Users
+ */
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { dbConnect } from "../../../utils/connection";
+import { ResponseFunctions } from '../../../interfaces';
+import User from '../../../models/User';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  // Identify the request method.
+  const method: keyof ResponseFunctions = req.method as keyof ResponseFunctions;
+
+  // Handle response. Note: For this project, we won't be creating POST request for User object.
+  const handleResponse: ResponseFunctions = {
+    // GET: All Users
+    GET: async (req: NextApiRequest, res: NextApiResponse) => {
+        // Connect to database.
+        await dbConnect();
+
+        // Fetch all users data from database.
+        const users = await User.find({}).catch((error: Error) => res.status(400).json({ error }));
+        
+        // Return response upon 200 status.
+        res.status(200).json(users);
+    }
+  }
+
+  // Return response if request method is exist/valid, otherwise return 400.
+  const response = handleResponse[method]
+  if (response) { 
+    response(req, res) 
+  } else  {
+    res.status(400).json({ error: "No response found, verify request method." })
+  }
+}
+
+export default handler;
