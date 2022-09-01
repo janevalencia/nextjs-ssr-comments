5b8dfe31da0200d584237b55a71dc1b07eb853dc
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-09-01 22:13:58 +0800

    Add interfaces for ResponseFunction & User (props)

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,29 +1,49 @@
-/**
- * Interfaces used when defining backend schema & models
- * - models/Comment
- * - models/Reply
- * - models/User
- */
-import { Types } from "mongoose"
+// Interface used to define backend schema and model.
+import { Date, Types } from "mongoose"
 
 export interface IUser {
-    username: string;
-    firstname: string;
-    lastname: string;
-    imageURL: string;
+    username: string
+    firstname: string
+    lastname: string
+    imageURL: string
 }
 
 export interface IComment {
-    content: string;
-    score: number;
-    user: Types.ObjectId;
-    replies: [Types.ObjectId];
+    content: string
+    score: number
+    user: Types.ObjectId
+    replies: [Types.ObjectId]
 }
 
 export interface IReply {
-    content: string;
-    score: number;
-    replyingTo: string;
-    user: Types.ObjectId;
-    parent: Types.ObjectId;
+    content: string
+    score: number
+    replyingTo: string
+    user: Types.ObjectId
+    parent: Types.ObjectId
 }
+// ============= END ===============
+
+
+// Interface used to define response object.
+export interface ResponseFunctions {
+  GET?: Function
+  POST?: Function
+  PUT?: Function
+  DELETE?: Function
+}
+// ============= END ===============
+
+
+// Interface to define models on the frontend component.
+export interface User {
+  _id?: number
+  username: string
+  firstname: string
+  lastname: string
+  imageURL: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+// ============= END ===============
