bd6556dcd2103d0a8c827a07bf4a984b22984710
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-09-02 00:52:38 +0800

    POST: User - create new user

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -10,19 +10,37 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // Identify the request method.
   const method: keyof ResponseFunctions = req.method as keyof ResponseFunctions;
 
-  // Handle response. Note: For this project, we won't be creating POST request for User object.
+  // Exception handler.
+  const exception = (error: Error) => res.status(400).json({ error });
+
+  // Handle response.
   const handleResponse: ResponseFunctions = {
-    // GET: All Users
+    // GET /users/: Fetch all users.
     GET: async (req: NextApiRequest, res: NextApiResponse) => {
         // Connect to database.
         await dbConnect();
 
         // Fetch all users data from database.
-        const users = await User.find({}).catch((error: Error) => res.status(400).json({ error }));
+        const users = await User.find({}).catch(exception);
         
-        // Return response upon 200 status.
+        // Return all users upon status 200.
         res.status(200).json(users);
-    }
+    },
+
+    // POST /users/: Create new user.
+    POST: async (req: NextApiRequest, res: NextApiResponse) => {
+        // Connect to database.
+        await dbConnect();
+
+        // Create a User object based on the request body.
+        const user = await User.create(req.body).catch(exception)
+
+        // Return newly created new user object upon status 200.
+        res.status(200).json({
+            "message" : "Successfully created a new user.",
+            "result" : user
+        });
+      },
   }
 
   // Return response if request method is exist/valid, otherwise return 400.
