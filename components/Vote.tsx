9bb59f4d4937c739b52fee2e3fa7e6c87a36c775
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-09-06 16:17:21 +0800

    Update Vote component to be displayed in reversed

--- components/Vote.tsx
+++ components/Vote.tsx
@@ -22,7 +22,7 @@ const Vote = ( { votes } : { votes : number } ) => {
     }
 
     return (
-        <div className="voting flex flex-row md:flex-col justify-center items-center text-center gap-5 md:gap-3 py-2 px-4 md:p-3 rounded-lg h-10 md:h-28 md:w-11">
+        <div className="voting flex flex-row-reverse md:flex-col justify-center items-center text-center gap-5 md:gap-3 py-2 px-4 md:p-3 rounded-lg h-10 md:h-28 md:w-11">
             <button className="button button__upvote" onClick={ upVote }>+</button>
             <span className="voting__digit">{vote}</span>
             <button className="button button__downvote" onClick={ downVote }>-</button>
